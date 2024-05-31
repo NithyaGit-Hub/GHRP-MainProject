503c2d2cd77c61bd5936672ca4d5288124120cdd
Nithya <Nithya@LAPTOP-5DETV3PI>
2024-05-31 18:29:35 +0530

    commit

--- GHRP-MainProject/src/Test.java
+++ GHRP-MainProject/src/Test.java
@@ -1,4 +1,11 @@
 
 public class Test {
 
+	public static void main(String args[])
+	{
+		System.out.println("**********");
+		System.out.println("Hello from GitHub");
+		System.out.println("**********");
+		
+	}
 }
